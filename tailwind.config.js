4a8dc2ee39134754ada6af2d18ded502ef104f36
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-02-07 12:47:08 -0300

    Extending tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '30': '7,5rem',
+        '290px': '290px',
+        '396px': '396px',
+        '500px': '500px',
+        '980px': '980px'
+      },
+      fontSize: {
+        'subtitle': '1.7rem'
+      },
+      colors: {
+        'main-gray': '#F0F2F5',
+        'main-blue': '#1877F2',
+        'main-blue-hover': '#166FE5',
+        'main-green': '#42B72A',
+        'main-green-hover': '#36A420'
+      }
+
+    },
   },
   plugins: [],
  }
