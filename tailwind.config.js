4b53664a4ca97b299e37313bf2ee416318d94ba1
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-02-07 13:12:22 -0300

    Add principal title

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '30': '7,5rem',
+        '30': '7.5rem',
         '290px': '290px',
         '396px': '396px',
         '500px': '500px',
